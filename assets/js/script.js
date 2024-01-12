ed9f316dfae773d633d9673abd2f429ad104b9ac
= <s3tha@icloud.com>
2024-01-11 21:17:56 -0600

    Hey look actual gameplay

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,6 @@ function fetchLocationData() {
         .then(response => response.json())
         .then(data => {
 
-            console.log(data)
-          
-            // const locationInfo = document.getElementById('locationInfo');
-            // locationInfo.innerHTML = `
-            //     <h1>Your Location Information</h1>
-            //     <p><strong>IP:</strong> ${data.ip}</p>
-            //     <p><strong>City:</strong> ${data.city}</p>
-            //     <p><strong>Region:</strong> ${data.region}</p>
-            //     <p><strong>Country:</strong> ${data.country}</p>
-            //     <p><strong>Location:</strong> ${data.loc}</p>
-            // `;
-
             var LatAndLong = data.loc.split(",")
 
             fetchWeatherData(LatAndLong[0],LatAndLong[1])
@@ -33,11 +21,13 @@ var BgImages = {
     Rain: "Rainy.png",
 }
 
+var DetectedWeather = "NO WEATHER SET"
+
 
 function fetchWeatherData(Lat, Long) {
     const apiKey = '6c80a9eb6cc84c6814a3ce34cdffcaa0';
 
-    var GetWeatherURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + Lat + '&lon=' + Long + '&appid=' + apiKey + '&units=imperial'
+    var GetWeatherURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + Lat + '&lon=' + Long + '&appid=' + apiKey + '&units=imperial'
 
 
 
@@ -50,6 +40,8 @@ function fetchWeatherData(Lat, Long) {
             var CurrWeather = data.list[0].weather[0].main
 
             console.log(CurrWeather)
+            DetectedWeather = CurrWeather
+
 
             if (BgImages[CurrWeather]) {
                 console.log("HEY LOOK IT FUCKIN' WORKED!")
@@ -71,4 +63,209 @@ function fetchWeatherData(Lat, Long) {
         });
 }
 
-// fetchWeatherData('New York');
+var ActualPet = document.getElementById("da-boi")
+var ActualPetImage = document.getElementById("pet-image")
+var PetOptions = document.getElementById("pet-actions")
+var FeedButton = document.getElementById("feed")
+var BrushButton = document.getElementById("brush")
+var PetButton = document.getElementById("pet")
+
+ActualPet.addEventListener("click", function(event) {
+
+    var Classes = PetOptions.classList
+    Classes.toggle("invisible")
+
+})
+
+var HappText = document.getElementById("happy-text")
+var HungryText = document.getElementById("hunger-text")
+
+// Get pet data
+
+var PetStats = localStorage.getItem('PetStats');
+
+if (PetStats) {
+
+    PetStats = JSON.parse(PetStats)
+
+} else {
+
+    PetStats = {
+        Hunger: 50,
+        Happiness: 50
+    }
+    
+}
+
+var GlobalButtonCooldown = false
+
+var AdjustStats = function(Hung, Hap) {
+
+    PetStats.Hunger = Math.min(100,Math.max(0,PetStats.Hunger + (Hung || 0)))
+    PetStats.Happiness = Math.min(100,Math.max(0,PetStats.Happiness + (Hap || 0))) 
+
+    if (GlobalButtonCooldown == false) {
+
+        UpdateImage()
+
+    }
+
+    HappText.textContent = "HAPPINESS: " + PetStats.Happiness + "/100"
+    HungryText.textContent = "HUNGER: " + PetStats.Hunger + "/100"
+
+    localStorage.setItem('PetStats', JSON.stringify(PetStats))
+
+}
+
+var UpdateImage = function() {
+
+    ActualPetImage.src = "./assets/Sprites/AlobeatBeansGoBrrr.png"
+
+}
+
+// Updates the text, because I'm too lazy to call two functions -Seth
+AdjustStats()
+
+function GlobalCD() {
+
+    GlobalButtonCooldown = true
+
+    var OrigCDTime = 1
+    var CDTime = OrigCDTime
+
+    var timeInterval = setInterval(function () {
+
+        if (CDTime <= OrigCDTime) {
+            UpdateImage()
+        }
+        
+        CDTime -= 1
+
+        if (CDTime <= 0) {
+
+        GlobalButtonCooldown = false    
+        clearInterval(timeInterval)
+
+        }
+    
+    }, 1000);
+
+}
+
+FeedButton.addEventListener("click", function(event) {
+
+    var Classes = FeedButton.classList
+
+    if (Classes.contains("opacity-100") && GlobalButtonCooldown == false) {
+
+        GlobalCD()
+
+        AdjustStats(10)
+
+        Classes.toggle("opacity-100")
+        Classes.toggle("opacity-50")
+
+        ActualPetImage.src = "./assets/Sprites/popseth.gif"
+
+        var CDTime = 2
+
+        var timeInterval = setInterval(function () {
+            
+            CDTime -= 1
+
+            if (CDTime <= 0) {
+
+                
+            Classes.toggle("opacity-50")
+            Classes.toggle("opacity-100")
+            clearInterval(timeInterval)
+
+            }
+        
+        }, 1000);
+
+    }
+
+})   
+
+BrushButton.addEventListener("click", function(event) {
+
+    var Classes = BrushButton.classList
+
+    if (Classes.contains("opacity-100") && GlobalButtonCooldown == false) {
+
+        GlobalCD()
+
+        AdjustStats(0,((DetectedWeather == "Rain" && 20) || 7))
+
+        Classes.toggle("opacity-100")
+        Classes.toggle("opacity-50")
+
+        ActualPetImage.src = "./assets/Sprites/SethToothbrush.gif"
+
+        var CDTime = 2
+
+        var timeInterval = setInterval(function () {
+            
+            CDTime -= 1
+
+            if (CDTime <= 0) {
+
+                
+            Classes.toggle("opacity-50")
+            Classes.toggle("opacity-100")
+            clearInterval(timeInterval)
+
+            }
+        
+        }, 1000);
+
+    }
+
+})  
+
+PetButton.addEventListener("click", function(event) {
+
+    var Classes = PetButton.classList
+
+    if (Classes.contains("opacity-100") && GlobalButtonCooldown == false) {
+
+        GlobalCD()
+
+        AdjustStats(0,10)
+
+        Classes.toggle("opacity-100")
+        Classes.toggle("opacity-50")
+
+        ActualPetImage.src = "./assets/Sprites/petdaseth.gif"
+
+        var CDTime = 2
+
+        var timeInterval = setInterval(function () {
+            
+            CDTime -= 1
+
+            if (CDTime <= 0) {
+
+                
+            Classes.toggle("opacity-50")
+            Classes.toggle("opacity-100")
+            clearInterval(timeInterval)
+
+            }
+        
+        }, 1000);
+
+    }
+
+}) 
+
+// Depletes stats overtime, set "Deplete Time" to the amount of seconds you want it to update with
+
+var StatDepletetime = 1
+
+setInterval(function () {
+
+    AdjustStats(-1,-2)
+
+}, StatDepletetime * 1000);
