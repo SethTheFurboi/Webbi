ae9e589dd7bf0ec049ace9f2e99ad0b6fa14da56
GinaKutz <gjkutz@gmail.com>
2024-01-16 18:35:39 -0600

    css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
 function fetchLocationData() {
-    fetch('https://ipinfo.io/json?token=YOUR_API_TOKEN')
+    fetch('https://ipinfo.io/[IP address]?token=7123ef64da11b7')
         .then(response => response.json())
         .then(data => {
           
-            const locationInfo = document.getElementById('locationInfo');
-            locationInfo.innerHTML = `
-                <h1>Your Location Information</h1>
-                <p><strong>IP:</strong> ${data.ip}</p>
-                <p><strong>City:</strong> ${data.city}</p>
-                <p><strong>Region:</strong> ${data.region}</p>
-                <p><strong>Country:</strong> ${data.country}</p>
-                <p><strong>Location:</strong> ${data.loc}</p>
-            `;
+            // const locationInfo = document.getElementById('locationInfo');
+            // locationInfo.innerHTML = `
+            //     <h1>Your Location Information</h1>
+            //     <p><strong>IP:</strong> ${data.ip}</p>
+            //     <p><strong>City:</strong> ${data.city}</p>
+            //     <p><strong>Region:</strong> ${data.region}</p>
+            //     <p><strong>Country:</strong> ${data.country}</p>
+            //     <p><strong>Location:</strong> ${data.loc}</p>
+            // `;
+
+            fetchWeatherData(data.city)
         })
         .catch(error => {
             console.error('Error fetching location data:', error);
@@ -27,15 +29,17 @@ window.onload = fetchLocationData;
 
 
 function fetchWeatherData(city) {
-    const apiKey = 'YOUR_WEATHER_API_KEY';
-    const apiUrl = `https://api.example.com/weather?city=${city}&apikey=${apiKey}`;
+    const apiKey = 'a3d03e21dae3e7499210cb56d3c6f61d';
+    const apiUrl = `https://api.openweathermap.org/weather?city=${city}&apikey=${apiKey}`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
         
             const weatherCode = data.weatherCode;
-        
+            
+            console.log (weatherCode)
+
             if (weatherCode === 'rain') {
                 document.body.style.backgroundImage = "url('rain.jpg')";
             } else if (weatherCode === 'clear') {
@@ -51,4 +55,4 @@ function fetchWeatherData(city) {
         });
 }
 
-fetchWeatherData('New York');
+// fetchWeatherData('New York');
